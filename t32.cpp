3cd91b93bae47f2db163c005bd22d65e72d62c96
p2rv <p2rv@yandex.ru>
2017-10-04 15:24:31 +1000

    Update t32.cpp

--- t32.cpp
+++ t32.cpp
@@ -1,34 +1,39 @@
-// t32.cpp: определяет точку входа для консольного приложения.
-//
-
 #include <iostream>
 #include <string>
+#include <cctype>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
+string ToLower(string s)
+{
+    for(auto& c:s)
+        c=tolower(c);
+    return s;
+}
 bool mysort(string i,string j)
 {
-	transform(i.begin(), i.end(), i.begin(), tolower);
-	transform(j.begin(), j.end(), j.begin(), tolower);
-	return i<j;
+    i=ToLower(i);
+    j=ToLower(j);
+    //transform(i.begin(), i.end(), i.begin(), tolower);
+    //transform(j.begin(), j.end(), j.begin(), tolower);
+    return i<j;
 }
 int main()
 {
-	int q;
-	cin >> q;
-	vector<string> array(q);
-	
-	for (auto& s : array)
-		cin >> s;
-		
-	sort(begin(array), end(array), mysort);
-	for (const auto& s : array)
-	{
-		cout << s << " ";
-	}
-	cout << endl;
-	//cin >> q;
+    int q;
+    cin >> q;
+    vector<string> array(q);
+
+    for (auto& s : array)
+        cin >> s;
+
+    sort(begin(array), end(array), mysort);
+    for (const auto& s : array)
+    {
+        cout << s << " ";
+    }
+    cout << endl;
+    //cin >> q;
     return 0;
 }
-
